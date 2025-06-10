9d034b7f54475059880d3b2c7352fb9d0d3150f7
NathanP06 <nathanjpugh6@gmail.com>
2025-06-09 20:56:43 -0400

    Added compatibility for multiple accounts & SSN getting.

--- src/Customer.java
+++ src/Customer.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
 
 public class Customer {
     private String name;
     private String ssn;
+    private ArrayList<Account> accounts;
 
     public Customer(String name, String ssn){
         this.name = name;
         this.ssn = ssn;
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public String getSSN(){
+        return ssn;
+    }
+
+    public String getMaskedSSN(){
+        return "***-**-" + ssn.substring(ssn.length() - 4);
     }
 
 }
