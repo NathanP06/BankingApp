715a4b3f9d5305ac8aa62b5edbf4ea4b18f2dfe5
NathanP06 <nathanjpugh6@gmail.com>
2025-06-09 20:49:52 -0400

    Created new "Customer" class

---
+++ src/Customer.java
@@ -0,0 +1,12 @@
+
+public class Customer {
+    private String name;
+    private String ssn;
+
+    public Customer(String name, String ssn){
+        this.name = name;
+        this.ssn = ssn;
+    }
+
+}
+
